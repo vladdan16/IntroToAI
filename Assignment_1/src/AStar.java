bc81007c4db24e59defc016f1e4b902bfadb9307
vladdan16 <v.danshov@innopolis.university>
2022-10-22 01:43:35 +0300

    qweasd

--- Assignment_1/src/AStar.java
+++ Assignment_1/src/AStar.java
@@ -1,16 +1,149 @@
 import java.io.PrintWriter;
+import java.util.*;
 
 public class AStar {
-    private int cost;
     private Map map;
+    private AStarNode[][] myMap;
     private final PrintWriter writer;
+    private PriorityQueue<AStarNode> queue;
+    private Set<AStarNode> closedSet;
+    private AStarNode initialNode;
+    private AStarNode dest;
+    private boolean tortuga;
 
     public AStar(Map map, PrintWriter writer) {
+        queue = new PriorityQueue<>();
+        closedSet = new HashSet<>();
         this.map = map;
         this.writer = writer;
+        myMap = new AStarNode[9][];
+        for (int i = 0; i < 9; i++) {
+            myMap[i] = new AStarNode[9];
+            for (int j = 0; j < 9; j++) {
+                myMap[i][j] = new AStarNode(map.getNode(i, j));
+            }
+        }
     }
 
     public void compute() {
-        
+        initialNode = myMap[map.getJack().x][map.getJack().y];
+        initialNode.setG(0);
+        dest = myMap[map.getChest().x][map.getChest().y];
+        initialNode.calculateH(dest);
+        queue.add(initialNode);
+        List<AStarNode> pathWithoutTortuga = findPath();
+        int pathLengthWithoutTortuga = pathWithoutTortuga.size() - 1;
+        if (pathLengthWithoutTortuga == -1)
+            pathLengthWithoutTortuga = 81;
+        List<AStarNode> pathWithTortuga = new ArrayList<>();
+        int pathLengthWithTortuga = 81;
+        if (pathLengthWithoutTortuga > Math.max(Math.abs(map.getJack().x - map.getTortuga().x), Math.abs(map.getJack().y - map.getTortuga().y)) + Math.max(Math.abs(map.getTortuga().x - map.getChest().x), Math.abs(map.getTortuga().y - map.getChest().y))) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    myMap[i][j] = new AStarNode(map.getNode(i, j));
+                }
+            }
+            dest = myMap[map.getTortuga().x][map.getTortuga().y];
+            initialNode.calculateH(dest);
+            queue.clear();
+            queue.add(initialNode);
+            closedSet.clear();
+            pathWithTortuga = findPath();
+            pathLengthWithTortuga = pathWithTortuga.size() - 1;
+            if (pathLengthWithTortuga == -1)
+                pathLengthWithTortuga = 81;
+            if (pathLengthWithTortuga < pathLengthWithoutTortuga) {
+                for (int i = 0; i < 9; i++) {
+                    for (int j = 0; j < 9; j++) {
+                        myMap[i][j] = new AStarNode(map.getNode(i, j));
+                    }
+                }
+                initialNode = myMap[map.getTortuga().x][map.getTortuga().y];
+                dest = myMap[map.getChest().x][map.getChest().y];
+                initialNode.calculateH(dest);
+                queue.clear();
+                queue.add(initialNode);
+                closedSet.clear();
+                List<AStarNode> t = findPath();
+                pathWithTortuga.addAll(t.subList(1, t.size()));
+                pathLengthWithTortuga = pathWithTortuga.size() - 1;
+                if (pathLengthWithTortuga == -1)
+                    pathLengthWithTortuga = 81;
+            }
+        }
+        if (Math.min(pathLengthWithoutTortuga, pathLengthWithTortuga) > 80) {
+            writer.println("Lose");
+            return;
+        }
+        writer.println("Win");
+        if (pathLengthWithoutTortuga <= pathLengthWithTortuga) {
+            writer.println(pathLengthWithoutTortuga);
+            displayResult(pathWithoutTortuga);
+        } else {
+            writer.println(pathLengthWithTortuga);
+            displayResult(pathWithTortuga);
+        }
+    }
+
+    private List<AStarNode> findPath() {
+        while (!queue.isEmpty()) {
+            AStarNode curNode = queue.poll();
+            if (curNode == dest) {
+                return getPath(curNode);
+            } else {
+                for (int x = Math.max(0, curNode.getNode().x - 1); x <= Math.min(8, curNode.getNode().x + 1); x++) {
+                    for (int y = Math.max(0, curNode.getNode().y - 1); y <= Math.min(8, curNode.getNode().y + 1); y++) {
+                        if (myMap[x][y].equals(curNode))
+                            continue;
+                        if (!(myMap[x][y].getG() > curNode.getG() + 1))
+                            continue;
+                        if (queue.contains(myMap[x][y]) && myMap[x][y].getG() < curNode.getG() + 1)
+                            continue;
+                        if (closedSet.contains(myMap[x][y]) && myMap[x][y].getG() < curNode.getG() + 1)
+                            continue;
+                        myMap[x][y].setData(curNode);
+                        myMap[x][y].calculateH(dest);
+                        queue.add(myMap[x][y]);
+                    }
+                }
+                closedSet.add(curNode);
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    private List<AStarNode> getPath(AStarNode node) {
+        List<AStarNode> path = new ArrayList<>();
+        path.add(node);
+        AStarNode parent;
+        while ((parent = node.getParent()) != null) {
+            path.add(0, parent);
+            node = parent;
+        }
+        return path;
+    }
+
+    private void displayResult(List<AStarNode> path) {
+        for (AStarNode e : path) {
+            writer.printf("[%d,%d] ", e.getNode().x, e.getNode().y);
+        }
+        writer.println();
+        writer.println("-------------------");
+        writer.println("  0 1 2 3 4 5 6 7 8");
+        for (int i = 0; i < 9; i++) {
+            for (int j = -1; j < 9; j++) {
+                if (j == -1) {
+                    writer.printf("%d ", i);
+                } else {
+                    if (path.contains(myMap[i][j])) {
+                        writer.printf("* ");
+                    } else {
+                        writer.printf("- ");
+                    }
+                }
+            }
+            writer.println();
+        }
+        writer.println("-------------------");
     }
 }
--- Assignment_1/src/AStarNode.java
+++ Assignment_1/src/AStarNode.java
@@ -3,12 +3,49 @@ public class AStarNode implements Comparable<AStarNode>{
     private int f;
     private int g;
     private int h;
+    private AStarNode parent;
 
     public AStarNode(Node node) {
         this.node = node;
         g = 81;
         h = 81;
+        f = 81;
+    }
+
+    public void calculateH(AStarNode dest) {
+        h = Math.max(Math.abs(node.x - dest.getNode().x), Math.abs(node.y - dest.getNode().y));
         computeF();
+    }
+
+    public void setData(AStarNode parent) {
+        setG(parent.g + 1);
+        setParent(parent);
+        computeF();
+    }
+
+    public boolean isShorterPath(AStarNode node) {
+        int g = node.g + 1;
+        if (g < this.g) {
+            setData(node);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        AStarNode node = (AStarNode) o;
+        return this.getNode().x == node.getNode().x && this.getNode().y == node.getNode().y;
+    }
+
+    public int getF() {
+        return f;
+    }
+    public int getG() {
+        return g;
+    }
+    public int getH() {
+        return h;
     }
 
     public void setG(int g) {
@@ -19,17 +56,26 @@ public class AStarNode implements Comparable<AStarNode>{
         this.h = h;
         computeF();
     }
-
     private void computeF() {
         f = g + h;
+    }
+    public void setParent(AStarNode parent) {
+        this.parent = parent;
+    }
+    public AStarNode getParent() {
+        return parent;
+    }
+
+    public Node getNode() {
+        return node;
     }
 
     @Override
     public int compareTo(AStarNode o) {
         if (this.f < o.f) {
-            return 1;
-        } else if (this.f > o.f) {
             return -1;
+        } else if (this.f > o.f) {
+            return 1;
         } else {
             return Integer.compare(o.h, this.h);
         }
